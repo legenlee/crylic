5830f809a605717b4b4aabf0adf3ce77c0a8458f
LegenLee <lsko1332@naver.com>
2025-11-22 22:08:50 +0900

    fix: Rewrite missing navigation drawer

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-
+import { ROUTE_NAMES } from './routes';
 </script>
 
 <template>
   <VApp>
+    <VNavigationDrawer permanent>
+      <VList>
+        <VListItem :to="{ name: ROUTE_NAMES.HOME }">
+          <VListItemTitle>Home</VListItemTitle>
+        </VListItem>
+        <VListItem :to="{ name: ROUTE_NAMES.ABOUT }">
+          <VListItemTitle>About</VListItemTitle>
+        </VListItem>
+      </VList>
+    </VNavigationDrawer>
+
     <VMain>
       <RouterView />
     </VMain>
