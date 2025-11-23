c8c30a2eb09b56b7651ac9ef014e158a1dd99d68
LegenLee <lsko1332@naver.com>
2025-11-23 22:59:49 +0900

    feat: Publish basic profile detail page layout

--- src/renderer/views/ProfileDetailView.vue
+++ src/renderer/views/ProfileDetailView.vue
@@ -11,7 +11,9 @@ const formattedNow = date.format(new Date(), 'fullDateTime');
 <template>
   <VContainer fluid>
     <h1 class="text-h1">{{ params.id }}</h1>
+
     <VDivider class="my-4" />
+
     <div class="d-flex align-center ga-4">
       <VBtn variant="elevated" size="x-large" prepend-icon="mdi-launch" text="Launch"></VBtn>
       <VDivider vertical />
@@ -24,7 +26,42 @@ const formattedNow = date.format(new Date(), 'fullDateTime');
         <span>Total playtime:</span>
         <div>0 hour(s)</div>
       </div>
+      <VSpacer />
+
+      <VMenu>
+        <template #activator="{ props }">
+          <VBtn icon="mdi-dots-vertical" v-bind="props" variant="text" color="medium-emphasis" />
+        </template>
+
+        <VList density="compact">
+          <VListItem title="Open Profile Folder" prepend-icon="mdi-folder-open" link />
+          <VListItem class="text-error" title="Delete Profile" prepend-icon="mdi-delete" link />
+        </VList>
+      </VMenu>
+
     </div>
     <VDivider class="my-4" />
+
+    <VRow>
+      <VCol cols="6">
+        <VCard title="Overview" color="primary" variant="tonal"></VCard>
+      </VCol>
+
+      <VCol cols="6">
+        <VCard title="Worlds" subtitle="0 world(s) created" color="primary" variant="tonal"></VCard>
+      </VCol>
+
+      <VCol cols="4">
+        <VCard title="Resource Packs" subtitle="0 resource pack(s) installed" color="primary" variant="tonal"></VCard>
+      </VCol>
+
+      <VCol cols="4">
+        <VCard title="Shader Packs" subtitle="0 shader pack(s) installed" color="primary" variant="tonal"></VCard>
+      </VCol>
+
+      <VCol cols="4">
+        <VCard title="Mods" subtitle="0 mod(s) installed" color="primary" variant="tonal"></VCard>
+      </VCol>
+    </VRow>
   </VContainer>
 </template>
