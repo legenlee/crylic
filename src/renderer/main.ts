cf408190b789e5ad59c398f8625bac28c110594d
LegenLee <lsko1332@naver.com>
2025-12-01 19:59:23 +0900

    refactor: Move directory structure

---
+++ src/renderer/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+import { router } from "./plugins/router";
+import { pinia } from "./plugins/pinia";
+import { vuetify } from "./plugins/vuetify";
+
+import App from "./App.vue";
+
+import "./styles/index.css";
+
+createApp(App).use(router).use(pinia).use(vuetify).mount("#app");
