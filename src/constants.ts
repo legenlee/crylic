67eac0b46cebf5a60fe9a82834b5ba8ae0aaa96f
LegenLee <lsko1332@naver.com>
2025-12-11 17:27:09 +0900

    refactor: Remove unused constants file

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-export const MANIFEST_URL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json";
-
-export enum IPC_TWO_WAY_CHANNELS {
-  METADATA_GET_VERSIONS = "metadata:getVersions",
-  METADATA_GET_CACHED_VERSIONS = "metadata:getCachedVersions",
-}
