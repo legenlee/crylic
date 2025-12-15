7bed31572822fd3b97769f0dfe79edc11e8dfdca
LegenLee <lsko1332@naver.com>
2025-12-15 23:00:29 +0900

    fix: Remove group expansion panel from ProfileView

--- src/renderer/views/ProfilesView.vue
+++ src/renderer/views/ProfilesView.vue
@@ -55,91 +55,31 @@ const profiles = [
 
 <template>
   <VContainer>
-    <VExpansionPanels multiple static>
-      <VExpansionPanel title="Group 1">
-        <VExpansionPanelText>
-          <VRow>
-            <VCol
-              v-for="(profile, index) in profiles"
-              :key="index"
-              md="4"
-              lg="3"
-            >
-              <VCard variant="tonal" color="primary">
-                <div class="d-flex">
-                  <div class="align-self-center">
-                    <VAvatar
-                      border
-                      class="mt-4 mb-4 ml-4"
-                      rounded="lg"
-                      size="64"
-                    >
-                      <VImg></VImg>
-                    </VAvatar>
-                  </div>
+    <VRow>
+      <VCol v-for="(profile, index) in profiles" :key="index" md="4" lg="3">
+        <VCard variant="tonal" color="primary">
+          <div class="d-flex">
+            <div class="align-self-center">
+              <VAvatar border class="mt-4 mb-4 ml-4" rounded="lg" size="48">
+                <VImg></VImg>
+              </VAvatar>
+            </div>
 
-                  <div class="flex-fill">
-                    <VCardItem>
-                      <VCardTitle>
-                        <span>{{ profile.name }}</span>
-                      </VCardTitle>
-                      <VCardSubtitle v-if="profile.modPlatform">
-                        {{ profile.version }} ({{ profile.modPlatform }})
-                      </VCardSubtitle>
-                      <VCardSubtitle v-else>{{
-                        profile.version
-                      }}</VCardSubtitle>
-                    </VCardItem>
-                  </div>
-                </div>
-              </VCard>
-            </VCol>
-          </VRow>
-        </VExpansionPanelText>
-      </VExpansionPanel>
-
-      <VExpansionPanel title="Group 2">
-        <VExpansionPanelText>
-          <VRow>
-            <VCol
-              v-for="(profile, index) in profiles"
-              :key="index"
-              md="4"
-              lg="3"
-            >
-              <VCard variant="tonal" color="primary">
-                <div class="d-flex">
-                  <div class="align-self-center">
-                    <VAvatar
-                      border
-                      class="mt-4 mb-4 ml-4"
-                      rounded="lg"
-                      size="64"
-                    >
-                      <VImg></VImg>
-                    </VAvatar>
-                  </div>
-
-                  <div class="flex-fill">
-                    <VCardItem>
-                      <VCardTitle>
-                        <span>{{ profile.name }}</span>
-                      </VCardTitle>
-                      <VCardSubtitle v-if="profile.modPlatform">
-                        {{ profile.version }} ({{ profile.modPlatform }})
-                      </VCardSubtitle>
-                      <VCardSubtitle v-else>{{
-                        profile.version
-                      }}</VCardSubtitle>
-                    </VCardItem>
-                  </div>
-                </div>
-              </VCard>
-            </VCol>
-          </VRow>
-        </VExpansionPanelText>
-      </VExpansionPanel>
-    </VExpansionPanels>
+            <div class="flex-fill">
+              <VCardItem>
+                <VCardTitle>
+                  <span>{{ profile.name }}</span>
+                </VCardTitle>
+                <VCardSubtitle v-if="profile.modPlatform">
+                  {{ profile.version }} ({{ profile.modPlatform }})
+                </VCardSubtitle>
+                <VCardSubtitle v-else>{{ profile.version }}</VCardSubtitle>
+              </VCardItem>
+            </div>
+          </div>
+        </VCard>
+      </VCol>
+    </VRow>
   </VContainer>
 
   <CreateProfileDialog>
