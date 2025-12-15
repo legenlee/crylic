1cdb5035182b18b00feffd58e9a5bd8991ec7e1b
LegenLee <lsko1332@naver.com>
2025-12-15 20:54:39 +0900

    fix: Wrap layout components with VLayout in DefaultLayout.vue

--- src/renderer/layouts/DefaultLayout.vue
+++ src/renderer/layouts/DefaultLayout.vue
@@ -1,49 +1,48 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { useRouter } from "vue-router";
 import { RouteNames } from "../routes";
-
-const router = useRouter();
 
 const auth = ref(false);
 </script>
 
 <template>
-  <VNavigationDrawer permanent rail color="primary">
-    <VList nav density="compact">
-      <VListItem
-        link
-        :to="{ name: RouteNames.PROFILES }"
-        prepend-icon="mdi-apps"
-      />
-    </VList>
-
-    <template #append>
+  <VLayout>
+    <VNavigationDrawer permanent rail color="primary">
       <VList nav density="compact">
-        <VMenu v-if="auth">
-          <template #activator="{ props }">
-            <VListItem v-bind="props" prepend-icon="mdi-account" />
-          </template>
-
-          <VList density="compact">
-            <VListItem title="Sign out" prepend-icon="mdi-logout" />
-          </VList>
-        </VMenu>
-        <VMenu v-else>
-          <template #activator="{ props }">
-            <VListItem v-bind="props" prepend-icon="mdi-account" />
-          </template>
-
-          <VList density="compact">
-            <VListItem title="Sign In" prepend-icon="mdi-login" link />
-          </VList>
-        </VMenu>
-        <VListItem link prepend-icon="mdi-cog" />
+        <VListItem
+          link
+          :to="{ name: RouteNames.PROFILES }"
+          prepend-icon="mdi-apps"
+        />
       </VList>
-    </template>
-  </VNavigationDrawer>
 
-  <VMain>
-    <slot />
-  </VMain>
+      <template #append>
+        <VList nav density="compact">
+          <VMenu v-if="auth">
+            <template #activator="{ props }">
+              <VListItem v-bind="props" prepend-icon="mdi-account" />
+            </template>
+
+            <VList density="compact">
+              <VListItem title="Sign out" prepend-icon="mdi-logout" />
+            </VList>
+          </VMenu>
+          <VMenu v-else>
+            <template #activator="{ props }">
+              <VListItem v-bind="props" prepend-icon="mdi-account" />
+            </template>
+
+            <VList density="compact">
+              <VListItem title="Sign In" prepend-icon="mdi-login" link />
+            </VList>
+          </VMenu>
+          <VListItem link prepend-icon="mdi-cog" />
+        </VList>
+      </template>
+    </VNavigationDrawer>
+
+    <VMain>
+      <slot />
+    </VMain>
+  </VLayout>
 </template>
