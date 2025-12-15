5a16b7e5bb5bade805e9da73d990137a48f6d172
LegenLee <lsko1332@naver.com>
2025-12-15 22:58:26 +0900

    feat: Published new ProfilesView

--- src/renderer/views/ProfilesView.vue
+++ src/renderer/views/ProfilesView.vue
@@ -1,27 +1,145 @@
 <script setup lang="ts">
-import GridList from "../components/common/GridList.vue";
 import CreateProfileDialog from "../dialogs/CreateProfileDialog.vue";
+
+const profiles = [
+  {
+    name: "Test Profile",
+    version: "1.21.10",
+    modPlatform: "Fabric",
+  },
+  {
+    name: "Test Profile",
+    version: "1.16.5",
+    modPlatform: "Forge",
+  },
+  {
+    name: "Test Profile",
+    version: "1.21.10",
+  },
+  {
+    name: "Test Profile",
+    version: "1.21.1",
+    modPlatform: "Fabric",
+  },
+  {
+    name: "Test Profile",
+    version: "1.21.9",
+    modPlatform: "Fabric",
+  },
+  {
+    name: "Test Profile",
+    version: "1.21.10",
+  },
+  {
+    name: "Test Profile",
+    version: "1.21.1",
+    modPlatform: "NeoForge",
+  },
+  {
+    name: "Test Profile",
+    version: "1.21.10",
+    modPlatform: "Quilt",
+  },
+  {
+    name: "Test Profile",
+    version: "1.20.4",
+    modPlatform: "Fabric",
+  },
+  {
+    name: "Test Profile",
+    version: "1.21.1",
+    modPlatform: "Forge",
+  },
+];
 </script>
 
 <template>
-  <VContainer min-height="100%" fluid>
-    <GridList />
-    <!-- <VRow>
-      <VCol v-for="dummy in dummys" :key="dummy" cols="3">
-        <VCard :to="{ name: RouteNames.PROFILE_DETAIL, params: { id: dummy } }" flat>
-          <VCardItem>
-            <VCardTitle>{{ dummy }}</VCardTitle>
-            <VCardSubtitle>1.20.1</VCardSubtitle>
-          </VCardItem>
+  <VContainer>
+    <VExpansionPanels multiple static>
+      <VExpansionPanel title="Group 1">
+        <VExpansionPanelText>
+          <VRow>
+            <VCol
+              v-for="(profile, index) in profiles"
+              :key="index"
+              md="4"
+              lg="3"
+            >
+              <VCard variant="tonal" color="primary">
+                <div class="d-flex">
+                  <div class="align-self-center">
+                    <VAvatar
+                      border
+                      class="mt-4 mb-4 ml-4"
+                      rounded="lg"
+                      size="64"
+                    >
+                      <VImg></VImg>
+                    </VAvatar>
+                  </div>
 
-          <VCardActions>
-            <VSpacer />
-            <VBtn icon="mdi-delete" size="small" @click.prevent />
-            <VBtn variant="elevated" text="Launch" prepend-icon="mdi-launch" @click.prevent />
-          </VCardActions>
-        </VCard>
-      </VCol>
-    </VRow> -->
+                  <div class="flex-fill">
+                    <VCardItem>
+                      <VCardTitle>
+                        <span>{{ profile.name }}</span>
+                      </VCardTitle>
+                      <VCardSubtitle v-if="profile.modPlatform">
+                        {{ profile.version }} ({{ profile.modPlatform }})
+                      </VCardSubtitle>
+                      <VCardSubtitle v-else>{{
+                        profile.version
+                      }}</VCardSubtitle>
+                    </VCardItem>
+                  </div>
+                </div>
+              </VCard>
+            </VCol>
+          </VRow>
+        </VExpansionPanelText>
+      </VExpansionPanel>
+
+      <VExpansionPanel title="Group 2">
+        <VExpansionPanelText>
+          <VRow>
+            <VCol
+              v-for="(profile, index) in profiles"
+              :key="index"
+              md="4"
+              lg="3"
+            >
+              <VCard variant="tonal" color="primary">
+                <div class="d-flex">
+                  <div class="align-self-center">
+                    <VAvatar
+                      border
+                      class="mt-4 mb-4 ml-4"
+                      rounded="lg"
+                      size="64"
+                    >
+                      <VImg></VImg>
+                    </VAvatar>
+                  </div>
+
+                  <div class="flex-fill">
+                    <VCardItem>
+                      <VCardTitle>
+                        <span>{{ profile.name }}</span>
+                      </VCardTitle>
+                      <VCardSubtitle v-if="profile.modPlatform">
+                        {{ profile.version }} ({{ profile.modPlatform }})
+                      </VCardSubtitle>
+                      <VCardSubtitle v-else>{{
+                        profile.version
+                      }}</VCardSubtitle>
+                    </VCardItem>
+                  </div>
+                </div>
+              </VCard>
+            </VCol>
+          </VRow>
+        </VExpansionPanelText>
+      </VExpansionPanel>
+    </VExpansionPanels>
   </VContainer>
 
   <CreateProfileDialog>
