a8f46a2706fb11fba104c84abcbe85640967e39f
LegenLee <lsko1332@naver.com>
2026-01-13 00:34:02 +0900

    refactor: Move layouts/DefaultLayout.vue to App.vue

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import DefaultLayout from "./layouts/DefaultLayout.vue";
+import { ref } from "vue";
+import { RouteNames } from "./routes";
+
+const auth = ref(false);
 </script>
 
 <template>
@@ -9,8 +12,46 @@ import DefaultLayout from "./layouts/DefaultLayout.vue";
       <VSpacer></VSpacer>
     </VSystemBar>
 
-    <DefaultLayout>
+    <VNavigationDrawer permanent rail color="primary">
+      <VList nav density="compact">
+        <VListItem
+          link
+          :to="{ name: RouteNames.PROFILES }"
+          prepend-icon="mdi-apps"
+        />
+      </VList>
+
+      <template #append>
+        <VList nav density="compact">
+          <VMenu v-if="auth">
+            <template #activator="{ props }">
+              <VListItem v-bind="props" prepend-icon="mdi-account" />
+            </template>
+
+            <VList density="compact">
+              <VListItem title="Sign out" prepend-icon="mdi-logout" />
+            </VList>
+          </VMenu>
+          <VMenu v-else>
+            <template #activator="{ props }">
+              <VListItem v-bind="props" prepend-icon="mdi-account" />
+            </template>
+
+            <VList density="compact">
+              <VListItem title="Sign In" prepend-icon="mdi-login" link />
+            </VList>
+          </VMenu>
+          <VListItem
+            link
+            :to="{ name: RouteNames.SETTINGS }"
+            prepend-icon="mdi-cog"
+          />
+        </VList>
+      </template>
+    </VNavigationDrawer>
+
+    <VMain>
       <RouterView />
-    </DefaultLayout>
+    </VMain>
   </VApp>
 </template>
