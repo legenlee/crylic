6f3d5269745e9a6a4ab90242acb8b44a8d0bcc90
LegenLee <lsko1332@naver.com>
2026-01-14 01:09:08 +0900

    refactor: Rename index.css to index.scss

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -5,6 +5,6 @@ import { vuetify } from "./plugins/vuetify";
 
 import App from "./App.vue";
 
-import "./styles/index.css";
+import "./styles/index.scss";
 
 createApp(App).use(router).use(pinia).use(vuetify).mount("#app");
