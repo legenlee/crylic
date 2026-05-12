e2a92da2ce795559970eed0c8328e3a6732a987a
LegenLee <lsko1332@naver.com>
2026-05-12 22:58:44 +0900

    refactor: Move all shareable config to ./config

---
+++ config/eslint.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "eslint";
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+
+export const jsRecommended = defineConfig(js.configs.recommended);
+
+export const tsRecommended = defineConfig(
+  js.configs.recommended,
+  ts.configs.recommended,
+);
+
+export const tsStrict = defineConfig(
+  js.configs.recommended,
+  ts.configs.recommended,
+);
