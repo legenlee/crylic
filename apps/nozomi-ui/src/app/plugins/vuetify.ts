e2162818adcc4717792cfe32ad83bacd65acc075
LegenLee <lsko1332@naver.com>
2026-05-19 21:41:35 +0900

    feat: Add Roboto font

--- apps/nozomi-ui/src/app/plugins/vuetify.ts
+++ apps/nozomi-ui/src/app/plugins/vuetify.ts
@@ -4,6 +4,20 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
+
+import "@fontsource/roboto/100-italic.css";
+import "@fontsource/roboto/300-italic.css";
+import "@fontsource/roboto/400-italic.css";
+import "@fontsource/roboto/500-italic.css";
+import "@fontsource/roboto/700-italic.css";
+import "@fontsource/roboto/900-italic.css";
+
 export const vuetify = createVuetify({
   components,
   directives,
