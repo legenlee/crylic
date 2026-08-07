a2a6c72e2a4bb70eaac43f5d52486cd2c2e67c1a
LegenLee <lsko1332@naver.com>
2026-08-07 12:39:28 +0900

    chore: Update eslint config in electron package

--- apps/nozomi-electron/eslint.config.ts
+++ apps/nozomi-electron/eslint.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "eslint/config";
 import { eslintConfig } from "@nozomi/config";
 import prettier from "eslint-config-prettier";
+import globals from "globals";
 
 export default defineConfig(
   {
-    ignores: ["**/dist"],
+    ignores: ["**/dist/**"],
     extends: [eslintConfig.tsStrict],
+    languageOptions: {
+      globals: globals.node,
+    },
   },
   prettier,
 );
