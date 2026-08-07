b17add0af3eba6ca1e8f65d117de6a27102e72ef
LegenLee <lsko1332@naver.com>
2026-08-07 14:23:48 +0900

    chore: Write preload script boilerplate

--- apps/nozomi-electron/src/preload.ts
+++ apps/nozomi-electron/src/preload.ts
@@ -1,1 +1,6 @@
 // Code for bridging context between renderer and main process.
+import { contextBridge } from "electron";
+
+contextBridge.exposeInMainWorld("nozomi", {
+  ping: () => console.log("pong"),
+});
