ff374037ab0f0c0607c6a81971f149fb4238d10f
LegenLee <lsko1332@naver.com>
2026-08-08 01:09:08 +0900

    chore: Attach watcher in electron dev script

--- apps/nozomi-electron/scripts/dev.ts
+++ apps/nozomi-electron/scripts/dev.ts
@@ -1,36 +1,109 @@
 import path from "node:path";
-import { spawn } from "node:child_process";
+import { ChildProcess, spawn } from "node:child_process";
+import { watch } from "rolldown";
 import { createServer } from "vite";
 import electron from "electron";
+// @ts-expect-error: Runs only in Node. Node cannot resolve scripts without extension.
+import config from "../rolldown.config.ts";
 
 const env = { ...process.env };
-const RENDERER_ROOT = path.resolve(import.meta.dirname, "../../nozomi-ui");
 
 delete env.ELECTRON_RUN_AS_NODE;
 
-process.chdir(RENDERER_ROOT);
-const server = await createServer({
-  root: RENDERER_ROOT,
+const root = path.resolve(import.meta.dirname, "../");
+const rendererRoot = path.resolve(import.meta.dirname, "../../nozomi-ui");
+const rootConfig = config.map((entry) => ({
+  ...entry,
+  cwd: root,
+  // Vite server will be startup. No need to copy the renderer bundle.
+  plugins: [],
+}));
+
+// Resolving vite-plugin-vue something
+process.chdir(rendererRoot);
+const rendererServer = await createServer({
+  root: rendererRoot,
 });
 
-await server.listen();
-server.printUrls();
+await rendererServer.listen();
+rendererServer.printUrls();
 
 // Electron exports a command line, but I had to force casting as string, since electron documented default export as a namespaced api.
 // Yeah, I know this is ugly.
-const child = spawn(electron as unknown as string, ["."], {
-  cwd: path.resolve(import.meta.dirname, "../"),
-  env: {
-    ...env,
-    VITE_DEV_SERVER_URL: server.resolvedUrls?.local[0],
-  },
-  stdio: "inherit",
-});
+const spawnProcess = () =>
+  spawn(electron as unknown as string, ["."], {
+    cwd: root,
+    env: {
+      ...env,
+      VITE_DEV_SERVER_URL: rendererServer.resolvedUrls?.local[0],
+    },
+    stdio: "inherit",
+  });
+
+type ProcessStatus = "normal" | "error" | "restarting";
+let processStatus: ProcessStatus = "normal";
+
+let child: ChildProcess | null = null;
+const watcher = watch(rootConfig);
 
 const exit = async () => {
-  await server.close();
+  await watcher.close();
+  await rendererServer.close();
+  child?.kill();
+
   process.exit(0);
 };
 
-child.on("close", exit);
-process.on("SIGINT", () => child.kill());
+const handleClose = () => {
+  if (processStatus === "restarting") {
+    return;
+  }
+
+  void exit();
+};
+
+const restart = async () => {
+  if (child) {
+    processStatus = "restarting";
+    const exited = () =>
+      new Promise((callback) => child?.once("close", callback));
+
+    child?.kill();
+    await exited();
+  }
+
+  processStatus = "normal";
+  child = spawnProcess();
+  child.on("close", handleClose);
+};
+
+watcher.on("event", async (event) => {
+  switch (event.code) {
+    case "START": {
+      processStatus = "normal";
+      break;
+    }
+
+    case "BUNDLE_END": {
+      await event.result.close();
+      break;
+    }
+
+    case "END": {
+      if (processStatus === "error") {
+        return;
+      }
+
+      await restart();
+      break;
+    }
+
+    case "ERROR": {
+      processStatus = "error";
+      console.error(event.error);
+      break;
+    }
+  }
+});
+
+process.on("SIGINT", exit);
